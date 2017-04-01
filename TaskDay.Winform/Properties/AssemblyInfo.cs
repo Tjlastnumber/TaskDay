69ac31700364dc3367dc3d60b2691877b7f47928
tjlastnumber <www.514163307@qq.com>
2017-04-02 02:08:39 +0800

    创建winform项目

--- TaskDay.Winform/Properties/AssemblyInfo.cs
+++ TaskDay.Winform/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("7a1b4a51-22c9-4729-bedf-33921456ff78")]
+[assembly: Guid("e00e531e-3bbb-416c-aba1-dffe74c53b2b")]
 
 // 程序集的版本信息由下面四个值组成: 
 //
