4af764af9c919bd98963f08726da667ff2b69a2d
tjlastnumber <www.514163307@qq.com>
2017-04-16 22:01:19 +0800

    增加string扩展类

---
+++ TaskDay.Core/Common/StringExpand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskDay.Core.Common
+{
+    public static class StringExpand
+    {
+        /// <summary>
+        /// 指示指定的字符串是 null 还是 System.String.Empty 字符串。
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static bool IsNullOrEmpty(this String s)
+        {
+            return string.IsNullOrEmpty(s);
+        }
+
+        /// <summary>
+        /// 指示指定的字符串是 null、空还是仅由空白字符组成。
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static bool IsNullOrWhiteSpace(this String s)
+        {
+            return string.IsNullOrWhiteSpace(s);
+        }
+    }
+}
