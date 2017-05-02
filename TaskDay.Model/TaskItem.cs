4fb79fba3afab0af501761c2247a30912d095bbc
tjlastnumber <www.514163307@qq.com>
2017-05-02 11:56:26 +0800

    添加：任务项模型

---
+++ TaskDay.Model/TaskItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskDay.Model
+{
+    public class TaskItem
+    {
+        /// <summary>
+        /// 是否完成
+        /// </summary>
+        public bool IsFinish { get; set; }
+        /// <summary>
+        /// 任务项内容
+        /// </summary>
+        public string Content { get; set; }
+    }
+}
